331ad4b13de212bf747ec743f1aecc333ce20d1c
Tom <tomhung87@gmail.com>
2016-03-14 22:55:57 -0700

    Add materializecss gem and files. Create controllers for sessions and users. Create view forms for sessions and users using materializecss. Create routes for sessions and users Add initial login and sign up links to application view. Add Simple_form Add create ability to sign up users through users#create

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email params[:email]
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Thank you for logging in"
+    else
+      flash[:notice] = "Username or password invalid"
+      render :new
+    end
+  end
+
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out!"
+  end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,15 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  # Users
+  resources :users, except: [:index, :new]
+  get '/signup' => 'users#new'
+
+  # Sessions
+  get '/login' => 'sessions#new', as: :login
+  delete '/logout' => "sessions#destroy", as: :logout
+  resources :sessions, only: [:create]
+
   # Example resource route with options:
   #   resources :products do
   #     member do
