5ce3feb74414f37a273b3fa5ccb67e7fc103a258
Sam Weatherly <weatherlysam@gmail.com>
2016-03-16 11:38:13 -0700

    handler for different flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(){
+  $('body').on('click', 'button.close', function(){
+    $(this).parent().slideUp(500);
+  });
+});
